a1e82ca68976c3a64e47283e42f53e5a303e9b33
Taeyoung Lee <tylee.fdcl@gmail.com>
2025-06-17 10:06:31 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,10 +12,25 @@ echo >> /Users/tylee/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/tylee/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+echo "Installing python nvim and tmux"
+
 brew install python3 neovim tmux
-brew install --cask git-credential-manager
 
 pip3 install --user --break-system-packages pynvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cd ~/.local/share/nvim/plugged/vim-markdown-composer
+cargo build --release
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Installing git credential manager"
+
+brew install --cask git-credential-manager
+
+echo "Copying config files"
 
 mkdir ~/.config
 
@@ -29,11 +44,5 @@ cp tmux.conf ~/.tmux.conf
 
 cp karabiner.json ~/.config/karabiner
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cd ~/.local/share/nvim/plugged/vim-markdown-composer
-cargo build --release
+echo "setup.sh completed"
 
